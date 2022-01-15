3ec13689b75243b4c55053254994153d62be4d39
Punit Jain <contactpunitjain@gmail.com>
2022-01-15 10:04:46 +0530

    creating a library using reveling pattern

---
+++ exercise1.js
@@ -0,0 +1,36 @@
+const convert = (function () {
+    // Convert grams to Kgs
+    function gramsToKg(grams) {
+        return parseFloat(grams) / 1000
+    }
+
+    // convert grams to milligrams
+    function gramsToMg(grams) {
+        return parseFloat(grams) * 1000
+    }
+
+    // convert kilograms to grams
+    function kgToGrams(kg) {
+        return parseFloat(kg) * 1000
+    }
+
+    // convert kiograms to milligrams
+    function kgToMg(kg) {
+        return kgToGrams(kg) * 1000
+    }
+
+    // convert milligrams to grams
+    function mgToGrams(mg) {
+        return parseFloat(mg) / 1000
+    }
+
+    // convert mg to kilograms
+    function mgToKg(mg) {
+        return mgToGrams / 1000
+    }
+
+    return { gramsToKg, gramsToMg, kgToGrams, kgToMg, mgToGrams, mgToKg }
+})()
+
+let mgs = convert.gramsToMg(42);
+console.log(mgs)
