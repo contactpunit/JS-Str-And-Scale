b31397c4be9786c3b16c3ce7ec4cded99cf36ea6
Punit Jain <contactpunitjain@gmail.com>
2022-01-15 11:25:13 +0530

    chaining methods

--- exercise2.js
+++ exercise2.js
@@ -15,9 +15,19 @@ const Weight = (function () {
         return this.weight
     }
 
+    Constructor.prototype.addMg = function (mg) {
+        this.weight = this.weight + parseFloat(mg)
+        return this
+    }
+
+    Constructor.prototype.addGrams = function (grams) {
+        this.weight = this.weight + (grams * 1000)
+        return this
+    }
+
     return Constructor
 })()
 
 let hummingbird = new Weight(4000)
-let grams = hummingbird.inGrams()
-console.log(grams)
+let afterEating = hummingbird.addMg(150).addGrams(1).inGrams()
+console.log(afterEating)
