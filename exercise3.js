c001a15bc788c7e82cd2b49b3fa2199b563613dd
Punit Jain <contactpunitjain@gmail.com>
2022-01-18 22:56:30 +0530

    constructor pattern

---
+++ exercise3.js
@@ -0,0 +1,41 @@
+const Convert = (function () {
+    function Constructor(weight, options = {}) {
+        const settings = Object.assign({
+            units: 'mg'
+        }, options)
+        if (!['mg', 'kg', 'grams'].includes(settings.units)) {
+            throw Error(`${settings.units} should have allowed values mg, kg and grams`)
+        }
+        this.weight = this.weightToMg(weight, settings.units)
+        console.log(this.weight)
+    }
+
+    Constructor.prototype.weightToMg = function (m, unit) {
+        if (unit === 'grams') return m * 1000
+        else if (unit === 'kg') return m * 1000 * 1000
+        else return m
+    }
+
+    Constructor.prototype.addMg = function (m) {
+        this.weight = this.weight + parseFloat(m)
+        return this
+    }
+
+    Constructor.prototype.addGrams = function (m) {
+        this.weight = this.weight + (m * 1000)
+        return this
+    }
+
+    Constructor.prototype.inGrams = function () {
+        return this.weight / 1000
+    }
+
+    return Constructor
+})()
+
+let hummingbird = new Convert(400, {
+    units: 'grams'
+})
+let bumbleBee = new Convert(150);
+let birdWeight = hummingbird.inGrams();
+console.log(birdWeight)
